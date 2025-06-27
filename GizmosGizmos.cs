f691dc14ad3346365419d42ab9e41df09f8be6db
Mai <mai@tilde.team>
2025-06-26 21:52:10 -0300

    initial commit, v0.1

---
+++ GizmosGizmos.cs
@@ -0,0 +1,29 @@
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using System.Collections.Generic;
+
+namespace GizmosGizmos
+{
+	public class GizmosGizmos: Mod
+	{
+    public override void AddRecipes() {
+      List<(short, short)> biomePairs = new List<(short, short)> {
+        (ItemID.CrimsonKey, ItemID.VampireKnives),
+        (ItemID.HallowedKey, ItemID.RainbowGun),
+        (ItemID.CorruptionKey, ItemID.ScourgeoftheCorruptor),
+        (ItemID.JungleKey, ItemID.PiranhaGun),
+        (ItemID.DungeonDesertKey, ItemID.StormTigerStaff),
+        (ItemID.FrozenKey, ItemID.StaffoftheFrostHydra)
+        };
+
+      foreach ((short keyId, short weaponId) in biomePairs) {
+        Recipe.Create(weaponId)
+          .AddIngredient(keyId)
+          .AddIngredient(ItemID.TempleKey)
+          .AddTile(TileID.DemonAltar)
+          .Register();
+      }
+    }
+	}
+}
